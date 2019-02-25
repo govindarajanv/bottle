55453135cbe289aa473b2421314033f35864e1d1
govindarajanv <govindarajanv@gmail.com>
2019-02-25 18:05:04 +0530

    commiting bottle framework exercises

---
+++ Bottle_error.py
@@ -0,0 +1,18 @@
+from bottle import run, route, error
+
+
+@route('/')
+def index():
+    return "Welcome to the Guest Longue"
+
+
+@error(404)
+
+:
+
+def not_found(error):
+    # if authorization fails
+    return "<h1>Not Found</h1>"
+
+
+run(host="localhost", port=1234, debug=True)
