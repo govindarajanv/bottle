c991ea7cc5d5de6d528213f0462a073d722cc251
govindarajanv <govindarajanv@gmail.com>
2019-03-05 06:42:12 +0530

    commiting bottle framework exercises

--- Bottle_plugins.py
+++ Bottle_plugins.py
@@ -25,23 +25,28 @@ def show_device(db, devicename):
 # http://localhost:1234/show?id=1&name=laptop
 
 @route('/show')
-def show_query():
+def show_query(db):
     os = request.query.os or None
     id = request.query.id or None
     name = request.query.name or None
 
     querystring = []
     if os:
-        querystring.append('os={}'.format(os))
+        querystring.append("os='{}'".format(os))
     if id:
         querystring.append('id={}'.format(id))
     if name:
-        querystring.append('name={}'.format(name))
+        querystring.append("name='{}'".format(name))
 
     if len(querystring) == 0:
         return "Invalid query"
     else:
-        return ' AND '.join(querystring)
-
-
+        query = "SELECT id,os,name from devices where {}".format(' AND '.join(querystring))
+        command = db.execute(query)
+        row = command.fetchone()
+        if row:
+            return template('{{id}},{{name}},{{os}}', id=row['id'], name=row['name'], os=row['os'])
+        else:
+            return template("The specified device with specifications {{name}}, does not exist",
+                            name=' AND '.join(querystring))
 run(host="localhost", port=1234, debug=True)
